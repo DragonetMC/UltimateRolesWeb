979b677ed73e9ffa8304d4365513849e1e6194b0
Soldier <1224954468@qq.com>
2017-09-24 23:28:52 +0800

    Add zh-cn

---
+++ system/application/lang/zh-cn.php
@@ -0,0 +1,36 @@
+<?php
+return [
+    "ur" => "终极角色",
+    "ur_slogan" => "一个完整的MC服务器网页角色/权限管理系统",
+
+    "login" => "登录",
+
+    "all" => "所有",
+    "create" => "创建",
+    "manage" => "管理",
+    "default" => "默认",
+    "desc" => "描述",
+
+    "failed" => "失败",
+    "success" => "成功",
+
+    "details" => "详情",
+
+    "pager_previous" => "上一页",
+    "pager_next" => "下一页",
+    "pager_display_label" => "页数",
+
+    "reset_password_help" => "如何重设密码",
+    "reset_password" => "重设密码",
+
+    // actions
+    "edit" => "编辑",
+    "delete" => "删除",
+    "reload" => "重载",
+
+    "name" => "ID",
+    "applied_to" => "应用于",
+    "actions" => "操作",
+    "type" => "类型",
+    "value" => "值",
+];
