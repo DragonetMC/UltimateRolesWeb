6c3c7b076e3d1a5f1514a4ad09a9da72575265c3
DefinitlyEvil <theevildays@gmail.com>
2017-09-27 00:59:04 +0800

    release 1.0

--- system/application/server/controller/ServerApi.php
+++ system/application/server/controller/ServerApi.php
@@ -7,6 +7,8 @@ namespace app\server\controller;
 use think\controller\Rest;
 
 class ServerApi extends Rest {
+    const VERSION = "1.0";
+
     public function __construct(){
         if(!isset($_GET["key"]) or $_GET["key"] !== config("security.secret_plugin")) {
             json(["status" => "error", "message" => "wrong_key"], 200)->send();
@@ -16,6 +18,6 @@ class ServerApi extends Rest {
     }
 
     protected function json($data = [], $status = "success", $message = "") {
-        return json(array_merge(["status" => $status, "message" => $message], $data), 200);
+        return json(array_merge(["status" => $status, "message" => $message, "version" => self::VERSION], $data), 200);
     }
 }
