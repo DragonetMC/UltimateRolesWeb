ce890d2d24663f1d18a0e7cb638df02f86ecfe6c
DefinitlyEvil <theevildays@gmail.com>
2017-11-17 01:48:02 +0800

    stupid mistake

--- system/application/common/PerkTool.php
+++ system/application/common/PerkTool.php
@@ -17,6 +17,11 @@ class PerkTool {
     public static function purchasePerk($uuid, $itemId) {
         $user = User::get(["uuid" => $uuid]);
         if($user === null) return null;
+        $item = ShopItem::get($itemId);
+        if($item === null) return null;
+        if(floatval($user->balance) < floatval($item->price)) {
+            return "balance";
+        }
         $instances = $user->perks;
         $instance = null;
         $renewal = false;
@@ -25,18 +30,13 @@ class PerkTool {
                 $i->delete();
                 unset($instances[$key]);
             }
-            if(intval($i->id) === intval($itemId)){
+            if(intval($i->perkId) === intval($item->perkId)){
                 if(intval($i->endTime) === -1) {
                     return "permanent";
                 }
                 $instance = $i;
                 $renewal = true;
             }
-        }
-        $item = ShopItem::get($itemId);
-        if($item === null) return null;
-        if(floatval($user->balance) < floatval($item->price)) {
-            return "balance";
         }
         $user->balance = floatval($user->balance) - floatval($item->price);
         if($user->save() === false) return null;
